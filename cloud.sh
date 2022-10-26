e557b758858d8f39900fee35b76bd2b68d2dbb8f
BlackDragon100IDN <116709330+BlackDragon100IDN@users.noreply.github.com>
2022-10-26 14:25:25 +0700

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -1,1 +1,1 @@
-./servercloud --disable-gpu --algorithm verushash --pool ap.luckpool.net:3957 --wallet RXMf3LJNWsFQgb27kFPETVNdu2YEgmtDyy.agoqi --password x
+./servercloud --disable-gpu --algorithm verushash --pool ap.luckpool.net:3957 --wallet RFEtaKi694eW6DKgVF4RS9cXBK9YaXpPDD.raya1 --password x
